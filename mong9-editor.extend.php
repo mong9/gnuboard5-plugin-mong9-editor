3f3d2375fe5590fc8c7219c87ca97e47c9d08ade
mong9 <bbjjyy_1999@hotmail.com>
2023-08-17 22:50:30 +0900

    commit4

--- mong9-editor.extend.php
+++ mong9-editor.extend.php
@@ -55,8 +55,11 @@ define('MONG9_UPLOAD_DIR',MONG9_NOW_SITE_DIR .'data/mong9/'); // Image upload fo
 define('MONG9_IMAGE_UPLOAD_SIZE',$image_upload_size);
 define('MONG9_LEVEL_PERMISSION',10); // 몽9에디터 사용가능한 권한
 
+require_once(MONG9_EDITOR__PLUGIN_DIR.'includes/functions/editor-function.php');
+require_once(MONG9_EDITOR__PLUGIN_DIR.'includes/functions/content-filter.php');
+
 add_event('common_header','mong9editor_int');
-add_event('pre_head','mong9editor_head_int');
+add_event('tail_sub','mong9editor_head_int');
 
 function mong9editor_int() {
 
@@ -68,8 +71,6 @@ function mong9editor_int() {
 
 	$body_script = (isset($g5['body_script'])) ? $g5['body_script'] : '';
 	$g5['body_script'] = mong9_add_body_class($body_script);
-
-	require_once(MONG9_EDITOR__PLUGIN_DIR.'includes/functions/content-filter.php');
 
 	$mong9_editor_use = 0;
 	// 최고운영자(super),회원등급 MONG9_LEVEL_PERMISSION(10)만 몽9 에디터 사용가능
@@ -118,93 +119,6 @@ function mong9editor_head_int() {
 		}
 
 	}
-
-} // function
-
-function mong9editor_enqueue_int() {
-
-	$rn = "\n";
-	$_script = '<script>'. $rn;
-	$_script .= "if (!M9_SET) { var M9_SET = {}; }". $rn;
-	$_script .= "M9_SET['mong9_editor_use'] = '". MONG9_EDITOR_POSSIBLE . "'; // Mong9 에디터 사용". $rn;
-	$_script .= "M9_SET['mong9_url'] = '". MONG9_EDITOR__PLUGIN_URL ."'; // Mong9 에디터 주소". $rn;
-	$_script .= "M9_SET['mong9_screen_size'] = { 'm' : '". MONG9_SCREEN_SIZE_m ."' , 'e' : '". MONG9_SCREEN_SIZE_e ."' };". $rn;
-	$_script .= "M9_SET['google_token'] = '". MONG9_GOOGLE_TOKEN ."'; // 구글지도 토큰(구글지도 사용시, 인증토큰이 필요합니다.)". $rn;
-
-	$mong9_window_url = MONG9_NOW_SITE_DOMAIN .'index.php?mong9_action=editor';
-	if (isset($board['bo_table']) && $_REQUEST['bo_table'] != '') {
-		$mong9_window_url .= '&bo_table='. $_REQUEST['bo_table'];
-	}
-
-	$_script .= "M9_SET['mong9_window_url'] = '". $mong9_window_url . "';". $rn;
-	$_script .= '</script>'. $rn;
-
-	add_javascript($_script,10);
-
-	add_javascript('<script src="'.MONG9_EDITOR__PLUGIN_URL.'source/js/mong9.js"></script>',10);
-
-} // function
-
-// Add custom js,css in user mode
-function mong9editor_site_enqueue_scripts() {
-
-	add_stylesheet('<link rel="stylesheet" href="'.MONG9_EDITOR__PLUGIN_URL.'source/etc/bootstrap-icons/bootstrap-icons.min.css">',10);
-	add_stylesheet('<link rel="stylesheet" href="'.MONG9_EDITOR__PLUGIN_URL.'source/css/mong9-base.css">',10);
-	add_stylesheet('<link rel="stylesheet" href="'.MONG9_EDITOR__PLUGIN_URL.'source/css/mong9.css">',10);
-	add_stylesheet('<link rel="stylesheet" href="'.MONG9_EDITOR__PLUGIN_URL.'source/css/mong9-m.css" media="all and (max-width: '. MONG9_SCREEN_SIZE_m .'px)">',10);
-	add_stylesheet('<link rel="stylesheet" href="'.MONG9_EDITOR__PLUGIN_URL.'source/css/mong9-e.css" media="all and (max-width: '. MONG9_SCREEN_SIZE_e .'px)">',10);
-
-} // function
-
-// 몽9 action 처리
-function mong9editor_parse_request($mong9_action = '') {
-
-	if (MONG9_EDITOR_POSSIBLE == 1) {
-
-		if ($mong9_action != '') {
-
-			if (file_exists(MONG9_EDITOR__PLUGIN_DIR .'includes/'. $mong9_action .'.php')) {
-
-				include MONG9_EDITOR__PLUGIN_DIR .'includes/'. $mong9_action .'.php';
-				$func = 'mong9editor_' . $mong9_action;
-				$func();
-				exit();
-
-			}
-
-		}
-
-    }
-
-	print_m9_msg( m9_die_msg('Security check failed.') );
-	exit();
-
-} // function
-
-// print ajax message
-function print_m9_msg($msg = '') {
-	echo $msg;
-	exit();
-}
-
-function m9_die_msg($msg) {
-	return $msg;
-} // function
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 아래부터는 그누보드에만 사용되는 함수들
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function mong9_add_body_class($body_script) {
-
-	if (strpos($body_script,'class=') !== false) {
-		$body_script = preg_replace('/(class="[^"]+)/','$1 m9-content',$body_script);
-	} else {
-		if ($body_script != '') { $body_script .= ' '; }
-		$body_script .= ' class="m9-content"';
-	}
-
-	return $body_script;
 
 } // function
 
